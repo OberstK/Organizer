73efb7d15eb75d48f9a98bc40258e0b375301961
DerHexer17 <hendrik.thues@gmail.com>
2014-01-25 12:06:15 +0100

    App Anbindung

---
+++ page/js/firstapp/controllers/classController.js
@@ -0,0 +1,7 @@
+var classController = firstApp.controller("classController", function($scope){
+	$scope.classes = [
+		{name: "EWI", prof: "Prof. Dr. Esswein", fachgebiet: "Winf"},
+		{name: "Unternehmenskommunikation", prof: "Prof. Dr. Schoop", fachgebiet: "Winf"},
+		{name: "Technologiemanagement", prof: "Prof. Dr. Schefzcyck", fachgebiet: "BWL"}
+	];
+});
