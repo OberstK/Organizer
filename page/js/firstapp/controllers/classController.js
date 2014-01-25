ae3778f7b3258126f4551cebfc59b884e594afe0
Benni <Benjamin.Kurczyk@gmail.com>
2014-01-25 18:43:12 +0100

    fix for Angular. Bootstrap added

--- page/js/firstapp/controllers/classController.js
+++ page/js/firstapp/controllers/classController.js
@@ -1,4 +1,4 @@
-var classController = firstApp.controller("classController", function($scope){
+var classController = firstApp.controller('classController', function($scope){
 	$scope.classes = [
 		{name: "EWI", prof: "Prof. Dr. Esswein", fachgebiet: "Winf"},
 		{name: "Unternehmenskommunikation", prof: "Prof. Dr. Schoop", fachgebiet: "Winf"},
