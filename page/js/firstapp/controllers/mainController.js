3a0585b54a96d0cfddbaa424a2ca83a748414f8b
DerHexer17 <hendrik.thues@gmail.com>
2014-01-26 15:19:43 +0100

    Tab-Navigation vorerst fertig

--- page/js/firstapp/controllers/mainController.js
+++ page/js/firstapp/controllers/mainController.js
@@ -1,13 +1,19 @@
 var mainController = firstApp.controller('mainController', function($scope){
-	$scope.classes = [
+	/*$scope.classes = [
 		{name: "EWI", prof: "Prof. Dr. Esswein", fachgebiet: "Winf"},
 		{name: "Unternehmenskommunikation", prof: "Prof. Dr. Schoop", fachgebiet: "Winf"},
 		{name: "Technologiemanagement", prof: "Prof. Dr. Schefzcyck", fachgebiet: "BWL"}
-	];
+	];*/
 
-	$scope.tabClick = function(){
-		
-		/*alert("Meine id ist: "+this.className);*/
+	$scope.tabClick = function(t){
+		var x = 1;
+		while(document.getElementById("tab"+x)){
+			if(document.getElementById("tab"+x).className="active"){
+				document.getElementById("tab"+x).className="";
+			}
+			x++;
+		}
+		document.getElementById("tab"+t).className="active";
 	}; 
 
 });
